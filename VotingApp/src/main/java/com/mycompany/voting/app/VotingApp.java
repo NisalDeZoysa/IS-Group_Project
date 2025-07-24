3d5266ff9c1fff2c7023864fbf038f23ec06b806
Inupa Vihan <inupavihan9@gmail.com>
2025-07-24 21:41:03 +0530

    Add Client Server Basic Communication GUI

---
+++ VotingApp/src/main/java/com/mycompany/voting/app/VotingApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.voting.app;
+
+/**
+ *
+ * @author Inupa Vihan
+ */
+public class VotingApp {
+
+    public static void main(String[] args) {
+        
+    }
+}
